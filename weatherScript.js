1f29a5c2d9540f48573b38740af795e279d34306
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-03-07 18:26:50 +0100

    Småfixes.

--- weatherScript.js
+++ weatherScript.js
@@ -9,6 +9,7 @@ let htmlElements = ["inputCity", "input-info", "output"];
 let getWeatherButton = document.getElementById("getWeatherButton");
 let inputCity = document.getElementById("inputCity");
 
+
 // let output = document.getElementById("output");
 
 const out = {
@@ -35,29 +36,37 @@ const getWeatherPressed = async function() {
     if(!weatherDetails)
     {
       //Then something went wrong
-      out.header.innerText = `Väderdata för '${city}' kunde inte hämtas, orsak okänd.`
+      out.header.innerHTML = `Väderdata för '${city}' kunde inte hämtas, orsak okänd.`
     } else if(weatherDetails.cod === '404') {
-      out.header.innerText = `Väderdata för '${city}' kunde inte hämtas, staden hittades inte.`
+      out.header.innerHTML = `Väderdata för '${city}' kunde inte hämtas, staden hittades inte.`
     } else {
 
       //all seems good, proceed.
-      
-      out.header.innerText = `Väderdata för '${city}' :`;
-      out.temp = `Nuvarande temperatur ${weatherDetails} ${} ${}`
-      out.latitude
+
+      out.header.innerHTML = `Väderdata för '${city}' :`;
+      out.temp.innerHTML = `Nuvarande temperatur ${kelvtinToCelcius(weatherDetails.main.temp)}°C, <br/>`+
+                           `Maxtemp: ${kelvtinToCelcius(weatherDetails.main.temp_max)}°C, <br/>`+
+                           `Mintemp: ${kelvtinToCelcius(weatherDetails.main.temp_min)}°C<br/>`+
+                           `Feels like ${kelvtinToCelcius(weatherDetails.main.feels_like)}°C`;
+      out.windspeed.innerHTML = `Vindhastighet ${weatherDetails.wind.speed}m/s`;
+      out.weather.innerHTML = `Väderbeskrivning: ${weatherDetails.weather[0].description}.`;
 
 
     }
     // setInfoText(weatherDetails);
   } else {
     //a city hasn't been inputted yet.
-    out.header.innerText = `Väderdata för ${city} kunde inte hämtas, orsak okänd.`;
+    out.header.innerHTML = `Väderdata för ${city} kunde inte hämtas, orsak okänd.`;
 
   }
 
   //Visa info-div (oavsett om lyckas eller misslyckas)
   output.style="display: block;"
 
+}
+
+const kelvtinToCelcius = function (kelvin) {
+  return Math.round(kelvin - 273.15);
 }
 
 
