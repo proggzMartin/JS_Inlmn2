ab7f8d06877569ccc7a874d49c0f50b7615001b5
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-03-12 20:39:55 +0100

    Errormessage to weather forfined, refactored out methods for resetting and setting text.

--- siteScript.js
+++ siteScript.js
@@ -1,53 +1,50 @@
 "use strict";
 
-import getWeather from './ExternalAPIs/openWeatherAPI.js';
-import isString from './typeChecks.js';
-import getVenues from './ExternalAPIs/fourSquareAPI.js';
-import createInputObject from './ExternalAPIs/createHTMLElements.js';
+import getWeather from "./ExternalAPIs/openWeatherAPI.js";
+import isString from "./typeChecks.js";
+import getVenues from "./ExternalAPIs/fourSquareAPI.js";
+import createInputObject from "./ExternalAPIs/createHTMLElements.js";
 
 //Vill bara ha det uppskrivet, samlat nånstans.
-let htmlElements = ["inputCity", "input-info", "weatherWrap"];
+let htmlElements = ["goButton", "inputCity", "input-info", "weatherWrap"];
 
 let inputCity = document.getElementById("inputCity");
-inputCity.value="Göteborg";
+inputCity.value = "Göteborg";
 
 let radioButtons = document.getElementById("radioButtons");
 let showWeather = true;
 let showAttractions = true;
 
-let radioIds = [ 
-  { 
+let radioIds = [
+  {
     title: "Only weather",
     func: () => {
       showWeather = true;
       showAttractions = false;
-    }
+    },
   },
-  { 
+  {
     title: "Only attractions",
     func: () => {
       showWeather = false;
       showAttractions = true;
-    } 
+    },
   },
-  { 
+  {
     title: "Filter alphabetically",
     func: () => {
       showWeather = true;
       showAttractions = true;
-    } 
-  }
+    },
+  },
 ];
 
-
-
 for (let i = 0; i < radioIds.length; i++) {
-
   var wrapDiv = document.createElement("div");
-  var radioAndLabel = createInputObject('radio', radioIds[i].title);
-  radioAndLabel.input.name = 'options';
+  var radioAndLabel = createInputObject("radio", radioIds[i].title);
+  radioAndLabel.input.name = "options";
 
-  radioAndLabel.input.addEventListener('change',radioIds[i].func);
+  radioAndLabel.input.addEventListener("change", radioIds[i].func);
 
   wrapDiv.appendChild(radioAndLabel.input);
   wrapDiv.appendChild(radioAndLabel.label);
@@ -55,66 +52,89 @@ for (let i = 0; i < radioIds.length; i++) {
   radioButtons.appendChild(wrapDiv);
 }
 
-
 const out = {
-  "weatherWrap": document.getElementById("weatherWrap"),
-  "header": document.getElementById("header"),
-  "temp": document.getElementById("temp"),
-  "windspeed": document.getElementById("windspeed"),
-  "weather": document.getElementById("weather"),
-}
+  weatherWrap: document.getElementById("weatherWrap"),
+  header: document.getElementById("header"),
+  temp: document.getElementById("temp"),
+  windspeed: document.getElementById("windspeed"),
+  weather: document.getElementById("weather"),
+};
 
-out.weatherWrap.style="display: none;"
+out.weatherWrap.style = "display: none;";
 
 const outputVisible = false;
 
-const getWeatherPressed = async function() {
 
+function hideWeatherDetails(errorMessage) {
+  out.header.innerHTML = errorMessage;
+  out.temp.innerHTML = "";
+  out.windspeed.innerHTML = "";
+  out.weather.innerHTML = "";
+}
+function setWeatherDetails(hideAll, weatherDetails) {
+    out.header.innerHTML = `Väderdata för '${city}' :`;
+    out.temp.innerHTML =
+      `Nuvarande temperatur ${kelvtinToCelcius(
+        weatherDetails.main.temp
+      )}°C, <br/>` +
+      `Maxtemp: ${kelvtinToCelcius(weatherDetails.main.temp_max)}°C, <br/>` +
+      `Mintemp: ${kelvtinToCelcius(weatherDetails.main.temp_min)}°C<br/>` +
+      `Feels like ${kelvtinToCelcius(weatherDetails.main.feels_like)}°C`;
+    out.windspeed.innerHTML = `Vindhastighet ${weatherDetails.wind.speed}m/s`;
+    out.weather.innerHTML = `Väderbeskrivning: ${weatherDetails.weather[0].description}.`;
+}
+const performSearch = async function () {
   let city = inputCity.value;
 
   //Checking against module typeChecks.js
-  if(isString(city)) {
-    const weatherDetails = await getWeather(city); //<--openWeatherAPI.js
-
-    //cod is received http code.
-    if(!weatherDetails)
-    {
-      //Then something went wrong
-      out.header.innerHTML = `Väderdata för '${city}' kunde inte hämtas, orsak okänd.`
-    } else if(weatherDetails.cod === '404') {
-      out.header.innerHTML = `Väderdata för '${city}' kunde inte hämtas, staden hittades inte.`
-    } else {
-
-      //all seems good, proceed.
-
-      out.header.innerHTML = `Väderdata för '${city}' :`;
-      out.temp.innerHTML = `Nuvarande temperatur ${kelvtinToCelcius(weatherDetails.main.temp)}°C, <br/>`+
-                           `Maxtemp: ${kelvtinToCelcius(weatherDetails.main.temp_max)}°C, <br/>`+
-                           `Mintemp: ${kelvtinToCelcius(weatherDetails.main.temp_min)}°C<br/>`+
-                           `Feels like ${kelvtinToCelcius(weatherDetails.main.feels_like)}°C`;
-      out.windspeed.innerHTML = `Vindhastighet ${weatherDetails.wind.speed}m/s`;
-      out.weather.innerHTML = `Väderbeskrivning: ${weatherDetails.weather[0].description}.`;
+  if (isString(city)) {
+    if (showWeather) {
+      const weatherDetails = await getWeather(city); //<--openWeatherAPI.js
 
 
+      console.log(weatherDetails);
+      //cod is received http code.
+      if (!weatherDetails)
+        //Then something went wrong
+        hideWeatherDetails(`Staden '${city}' kunde inte hämtas, orsak okänd.`);
+      else if (weatherDetails.cod === "404")
+        hideWeatherDetails(`Staden '${city}' kunde inte hämtas, staden hittades inte.`);
+      else {
+        //all seems good, proceed.
+
+        out.header.innerHTML = `Väderdata för '${city}' :`;
+        out.temp.innerHTML =
+          `Nuvarande temperatur ${kelvtinToCelcius(
+            weatherDetails.main.temp
+          )}°C, <br/>` +
+          `Maxtemp: ${kelvtinToCelcius(
+            weatherDetails.main.temp_max
+          )}°C, <br/>` +
+          `Mintemp: ${kelvtinToCelcius(weatherDetails.main.temp_min)}°C<br/>` +
+          `Feels like ${kelvtinToCelcius(weatherDetails.main.feels_like)}°C`;
+        out.windspeed.innerHTML = `Vindhastighet ${weatherDetails.wind.speed}m/s`;
+        out.weather.innerHTML = `Väderbeskrivning: ${weatherDetails.weather[0].description}.`;
+      }
+
+      if (showAttractions) {
+      }
+      // setInfoText(weatherDetails);
     }
-    // setInfoText(weatherDetails);
   } else {
     //a city hasn't been inputted yet.
     out.header.innerHTML = `Väderdata för ${city} kunde inte hämtas, orsak okänd.`;
-
   }
 
   //Visa info-div (oavsett om lyckas eller misslyckas)
-  weatherWrap.style="display: block;"
-
-}
+  weatherWrap.style = "display: block;";
+};
 
 const kelvtinToCelcius = function (kelvin) {
   return Math.round(kelvin - 273.15);
-}
+};
 
-getWeatherPressed();
+performSearch();
 
+let goButton = document.getElementById("goButton");
 
-
-
+goButton.onclick = performSearch;
